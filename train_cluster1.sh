3f629bb75c27e8968b977a9bcea163b97770acab
Yichi Zhang <zhangyic@umich.edu>
2023-11-05 23:32:48 +0000

    Update cluster training scripts

--- train_cluster1.sh
+++ train_cluster1.sh
@@ -2,7 +2,7 @@ export DETECTRON2_DATASETS=/data-fsx/xiaofeng/sg-mllm
 
 TOTAL_BATCH_SIZE=64 # 4 nodes * 8 gpus * 2 img/gpu
 
-EXP_NAME=swinl-all_data-q200+50+50-res1024-12ep-bs64-polylr
+EXP_NAME=swinl-all_data-q200+50+50-res1024-15ep-bs64-polylr
 python train_net_plus.py \
     --machine-rank $NODE_RANK \
     --num-machines $NUM_NODES  \
@@ -21,6 +21,6 @@ python train_net_plus.py \
     MODEL.MASK_FORMER.NUM_OBJECT_QUERIES 200 \
     MODEL.MASK_FORMER.NUM_PART_QUERIES 50 \
     MODEL.MASK_FORMER.NUM_TEXT_QUERIES 50 \
-    SOLVER.MAX_ITER 38000 \
+    SOLVER.MAX_ITER 50000 \
     SOLVER.LR_SCHEDULER_NAME WarmupPolyLR \
     TEST.EVAL_PERIOD 100000000
